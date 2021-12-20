a714cb2f85aa81a31b63cb0e8251f1b326589f67
Nanda_Kumar <42697805+nandakrr@users.noreply.github.com>
2021-12-20 21:30:15 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #
-# bash script to install Subdomain-Enumeration dependencies 
+# bash script to install Subdomain-Enumumeration dependencies 
 #
 
 GOlang() {
+	printf "                                \r"
 	sys=$(uname -m)
 	LATEST=$(curl -s 'https://go.dev/VERSION?m=text')
 	[ $sys == "x86_64" ] && wget https://golang.org/dl/$LATEST.linux-amd64.tar.gz -O golang.tar.gz &>/dev/null || wget https://golang.org/dl/$LATEST.linux-386.tar.gz -O golang.tar.gz &>/dev/null
@@ -16,9 +17,34 @@ GOlang() {
 }
 
 Findomain() {
+	printf "                                \r"
 	wget https://github.com/Edu4rdSHL/findomain/releases/latest/download/findomain-linux &>/dev/null
 	chmod +x findomain-linux
 	./findomain-linux -h &>/dev/null && { sudo mv findomain-linux /usr/local/bin/findomain; printf "[+] Findomain Installed !.\n"; } || printf "[!] Install Findomain manually: https://github.com/Findomain/Findomain/blob/master/docs/INSTALLATION.md\n"
+}
+
+Subfinder() {
+	printf "                                \r"
+	GO111MODULE=on go get -u -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder &>/dev/null
+	printf "[+] Subfinder Installed !.\n"
+}
+
+Amass() {
+	printf "                                \r"
+	GO111MODULE=on go get -v github.com/OWASP/Amass/v3/... &>/dev/null
+	printf "[+] Amass Installed !.\n"
+}
+
+Assetfinder() {
+	printf "                                \r"
+	go get -u github.com/tomnomnom/assetfinder &>/dev/null
+	printf "[+] Assetfinder Installed !.\n"
+}
+
+Httprobe() {
+	printf "                                \r"
+	go get -u github.com/tomnomnom/httprobe
+	printf "[+] Httprobe Installed !.\n"
 }
 
 hash go 2>/dev/null && printf "[!] Golang is already installed.\n" || { printf "[+] Installing GOlang!" && GOlang; } 
@@ -28,10 +54,18 @@ export GOPATH=$HOME/go
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 
 hash findomain 2>/dev/null && printf "[!] Findomain is already installed.\n" || { printf "[+] Installing Findomain!" && Findomain; }
+hash subfinder 2>/dev/null && printf "[!] subfinder is already installed.\n" || { printf "[+] Installing subfinder!" && Subfinder; }
+hash amass 2>/dev/null && printf "[!] Amass is already installed.\n" || { printf "[+] Installing Amass!" && Amass; }
+hash assetfinder 2>/dev/null && printf "[!] Assetfinder is already installed.\n" || { printf "[+] Installing Assetfinder!" && Assetfinder; }
+hash httprobe 2>/dev/null && printf "[!] Httprobe is already installed.\n" || { printf "[+] Installing Httprobe!" && Httprobe; }
 
 list=(
 	go
 	findomain
+	subfinder
+	amass
+	assetfinder
+	httprobe
 	)
 
 r="\e[31m"
